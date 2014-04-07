e881d8e6c47d2fe997ae32486288f23c8efe6472
Ashley Blackmore <ablackmore@atlassian.com>
2014-04-07 23:39:17 +1000

    add prompt.c

---
+++ prompt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Declare a static buffer for user input (max size 2048 chars) */
+static char input[2048];
+
+int main(int argc, char** argv) {
+    /* version and exit info */
+    puts("Lonesome Crowded Lisp 0.0.0.0.1");
+    return 0;
+}
