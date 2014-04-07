c7bac8f6638b6a256305bdbd74e2f1daab041e1a
Ashley Blackmore <ablackmore@atlassian.com>
2014-04-07 23:51:14 +1000

    basic echo prompt

--- prompt.c
+++ prompt.c
@@ -6,5 +6,18 @@ static char input[2048];
 int main(int argc, char** argv) {
     /* version and exit info */
     puts("Lonesome Crowded Lisp 0.0.0.0.1");
+    puts("Press Ctrl+c to exit\n");
+
+    while (1) {
+        /* Output a prompt */
+        fputs("(づ￣ ³￣)づ ", stdout);
+
+        /* Read a line of user input */
+        fgets(input, 2048, stdin);
+
+        /* Echo input back to user */
+        printf("Three inch horses, %s", input);
+    }
+
     return 0;
 }
