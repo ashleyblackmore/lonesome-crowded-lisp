adf2f09f96796b268ce9e5962f00726b579468b2
Ashley Blackmore <ablackmore@atlassian.com>
2014-04-08 22:01:06 +1000

    libedit modifications

--- prompt.c
+++ prompt.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a static buffer for user input (max size 2048 chars) */
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
     /* version and exit info */
     puts("Lonesome Crowded Lisp 0.0.0.0.1");
     puts("Press Ctrl+c to exit\n");
 
+    /* looooooop */
     while (1) {
-        /* Output a prompt */
-        fputs("(づ￣ ³￣)づ ", stdout);
+        /* output a prompt */
+        char* input = readline("(づ￣ ³￣)づ ");
+        add_history(input);
 
-        /* Read a line of user input */
-        fgets(input, 2048, stdin);
+        /* echo input back to user */
+        printf("Three inch horses, %s\n", input);
 
-        /* Echo input back to user */
-        printf("Three inch horses, %s", input);
+        /* free retrieved input */
+        free(input);
     }
 
     return 0;
